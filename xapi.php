e4f4e53e4c5d6ab17cd7fe3cf7fa4fe0c674403f
Adam R. Welle <arwelle@cert.org>
2023-03-24 11:57:55 -0400

    can successfully post a statement!

--- xapi.php
+++ xapi.php
@@ -3,6 +3,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Xabbuh\XApi\Client\XApiClientBuilder;
 use Xabbuh\XApi\Model\Statement;
+use Xabbuh\XApi\Model\Actor;
+use Xabbuh\XApi\Model\Agent;
+use Xabbuh\XApi\Model\StatementFactory;
+use Xabbuh\XApi\Model\InverseFunctionalIdentifier;
+use Xabbuh\XApi\Model\IRI;
+use Xabbuh\XApi\Model\Verb;
+use Xabbuh\XApi\Model\Activity;
+
+
 
 class xapi extends rcube_plugin
 {
@@ -19,7 +28,11 @@ class xapi extends rcube_plugin
 
 		// install user hooks
 		$this->add_hook('message_ready', array($this, 'log_sent_message'));
-		//$this->add_hook('message_read', [$this, 'log_read_message']);
+		$this->add_hook('message_read', [$this, 'log_read_message']);
+		//$this->add_hook('message_sent', [$this, 'log_sent_message']);
+		//$this->add_hook('login_after', [$this, 'log_login']);
+		//$this->add_hook('oauth_login', [$this, 'log_oauth_login']);
+		//$this->add_hook('refresh', [$this, 'log_refresh']);
 		
 	}
 
@@ -72,10 +85,75 @@ class xapi extends rcube_plugin
 		$this->build_client();
 		$statementsApiClient = $this->xApiClient->getStatementsApiClient();
 
-		$statement = new Statement(null, $actor, $verb, $object);
+		$actor = "arwelle";
+		$verb = "read";
+		$object = $args['message'];
+		//$statement = new Statement(null, $actor, $verb, $object);
 
 		// store a single Statement
-		$statementsApiClient->storeStatement($statement);
+		//$statementsApiClient->storeStatement($statement);
+
+		return $args;
+	}
+
+
+
+	public function log_read_message($args)
+	{
+		$db = rcmail::get_instance()->get_dbh();
+		$message = $args['message'];
+
+
+	        //Get user who is actually reading the email
+	        $rcmail = rcmail::get_instance();
+	        $user = $rcmail->user->get_username();
+	        $convert_user = $db->query("SELECT name FROM contacts WHERE email = '$user'");
+	        $records = $db->fetch_assoc($convert_user);
+	        $logged_user = $records['name'];
+
+	        // Get To User Value
+	        $to_orig = $message->get_header('to');
+	        $to_names = preg_replace('/<(.+?)>/', '', $to_orig);
+	        $to_names = preg_replace('/ , /', ',', $to_names);
+	        $to_names = trim($to_names);
+	        $to_array = explode(',', $to_names);
+
+	        // Get From User Value
+	        $from = $message->get_header('from');
+	        $convert_from = $db->query("SELECT name FROM contacts WHERE email = '$from'");
+	        $records = $db->fetch_assoc($convert_from);
+	        $from_name = $records['name'];
+
+	        // Get Subject Value
+	        $subject = $message->get_header('subject');
+	        $parsed_subject = substr($subject, strpos($subject, "]") + 2);
+	        // Get Date Value
+	        $date_str = $message->get_header('Date');
+	        $timestamp = date('Y-m-d H:i:s', strtotime($date_str)) . '+00';
+
+                // build xapi client
+                $this->build_client();
+                $statementsApiClient = $this->xApiClient->getStatementsApiClient();
+
+		foreach ($to_array as $to) {
+			$to = trim($to);
+
+
+			$actor = "arwelle";
+			$verb = "read";
+			$object = $args['message'];
+			$sf = new StatementFactory();
+			$sf->withActor(new Agent(InverseFunctionalIdentifier::withMbox(IRI::fromString('mailto:arwelle@cert.org'))));
+		        $sf->withVerb(new Verb(IRI::fromString('http://tincanapi.com/conformancetest/verbid')));
+		        $sf->withObject(new Activity(IRI::fromString('http://tincanapi.com/conformancetest/activityid')));
+
+			$statement = $sf->createStatement();
+			//$statement = new Statement(null, $actor, $verb, $object);
+
+			// store a single Statement
+			$statementsApiClient->storeStatement($statement);
+		}
+		// TODO maye all statements to array and send multiple at once right here
 
 		return $args;
 	}
